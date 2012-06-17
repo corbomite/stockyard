cef2ab0465ebed329b9a5ef1b9b365984abc5df3
Ted Nielsen <theodore.nielsen@gmail.com>
2012-06-16 19:28:56 -0500

    Heroku changes

--- edit.rb
+++ edit.rb
@@ -1,15 +1,15 @@
 require 'find'
 replacements = 0
 
-Find.find(".") do |path|
+Find.find("./public") do |path|
   if FileTest.directory?(path)
     puts "Skipping directory #{path}... \r\n"
     next
   else
     filename = File.basename(path)
-    if filename =~ /\.htm/
+    if filename =~ /\.htm/ or filename =~ /\.xml/
       s = IO.read(path) 
-      compliant = s.gsub(/stockyardmagazine/, "stockyardmag")
+      compliant = s.gsub(/http:\/\/www.stockyardmag/, "http:\/\/stockyardmag")
       replacements += 1 if s != compliant      
       File.open(path, 'w') {|f| f.write(compliant) }
     else
